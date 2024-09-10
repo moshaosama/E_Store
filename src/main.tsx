ca3313db1c5fe02e2d0dd5f3a50acfe650619564
moshaosama <mohamedOSFekry@gmail.com>
2024-09-10 03:43:43 +0300

    fasd

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { Store } from "./app/Store";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
